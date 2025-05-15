35c148d2663dd8420b246e44a01c9ec01a5f7348
Fábio Galvão <fgalvao90@gmail.com>
2025-05-15 21:32:36 +0200

    feat: Add campaigns

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,22 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\DonationController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [LoginController::class]);
+Route::post('login', LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::prefix('campaigns')->group(function() {
         Route::get('', [CampaignController::class, 'index'])->name('campaign.index');
         Route::post('', [CampaignController::class, 'store'])->name('campaign.store');
+        Route::get('/{campaign}', [CampaignController::class, 'show'])->name('campaign.show');
         Route::put('/{campaign}', [CampaignController::class, 'update'])->name('campaign.update');
         Route::delete('/{campaign}', [CampaignController::class, 'destroy'])->name('campaign.destroy');
+    });
+
+    Route::prefix('donations')->group(function() {
+        Route::get('/{donation}', [DonationController::class, 'show'])->name('donation.show');
     });
 
 });
